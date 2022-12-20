7b026f8c9e79a9b7b7887afee2f7b1466d1493f4
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-12-20 21:57:09 +0600

    reasons section style

--- src/components/Reasons/Reasons.js
+++ src/components/Reasons/Reasons.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import './Reasons.css'
+import './Reasons.css';
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
+import image4 from '../../assets/image4.png'
+import nb from '../../assets/nb.png';
+import adidas from '../../assets/adidas.png';
+import nike from '../../assets/nike.png';
+import tick from '../../assets/tick.png'
 const Reasons = () => {
     return (
-        <div>
-            Reasons
+        <div className='reasons' id='reasons'>
+            {/* left reasons */}
+            <div className='left-r'>
+                <img src={image1} alt="" />
+                <img src={image2} alt="" />
+                <img src={image3} alt="" />
+                <img src={image4} alt="" />
+            </div>
+
+            {/* Right reasons */}
+
+            <div className='right-r'>
+                <span>some reasons</span>
+                <div>
+                    <span className='stroke-text'>why</span>
+                    <span> choose us?</span>
+                </div>
+
+                {/* reasons details */}
+
+                <div className='details'>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>OVER 140+ EXPERT COACHS</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+                    </div>
+                    <div>
+                        <img src={tick} alt="" />
+                        <span>RELIABLE PARTNERS</span>
+                    </div>
+                </div>
+                {/* OUR PARTNERS */}
+
+                <span style={{ color: "var(--gray)" }}>OUR PARTNERS</span>
+
+                <div className='partners'>
+                    <img src={nb} alt="" />
+                    <img src={adidas} alt="" />
+                    <img src={nike} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
